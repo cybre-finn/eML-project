37c5e8c3f3734a2b0fbc09b5779308046e08bdb7
Finn <hochi@zer.one>
2024-01-15 22:55:40 +0100

    [eML project] edge impulse and servo work (curr. seperately)

--- main/main.cpp
+++ main/main.cpp
@@ -35,6 +35,18 @@
 
 #include "esp_camera.h"
 
+#include "driver/mcpwm_prelude.h"
+
+static const char *TAG = "my AI App";
+
+// SERVO DEFINITIONS
+#define SERVO_MIN_PULSEWIDTH_US 1000  // Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH_US 2000  // Maximum pulse width in microsecond
+#define SERVO_MIN_DEGREE        -90   // Minimum angle
+#define SERVO_MAX_DEGREE        90    // Maximum angle
+#define SERVO_PULSE_GPIO             2        // GPIO connects to the PWM signal line
+#define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
+#define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 
 // CAMERA DEFINITIONS FOR AI THINKER BOARD
 #define CAM_PIN_PWDN 32
@@ -60,6 +72,8 @@
 // My definitions 
 // TODO: Remove
 #define FEATURE_LEN 96*96
+
+
 // Init features to 0
 static const float features[FEATURE_LEN] = {0};
 
@@ -67,7 +81,6 @@ static const float features[FEATURE_LEN] = {0};
 static camera_fb_t *framebuffer_ptr;
 
 // CAMERA STUFF
-
 static camera_config_t camera_config = {
     .pin_pwdn = CAM_PIN_PWDN,
     .pin_reset = CAM_PIN_RESET,
@@ -100,6 +113,11 @@ static camera_config_t camera_config = {
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
+// a helper function for the servo; taken from the example page.
+static inline uint32_t example_angle_to_compare(int angle)
+{
+    return (angle - SERVO_MIN_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (SERVO_MAX_DEGREE - SERVO_MIN_DEGREE) + SERVO_MIN_PULSEWIDTH_US;
+}
 
 static esp_err_t init_camera(void)
 {
@@ -172,6 +190,68 @@ void print_inference_result(ei_impulse_result_t result) {
 
 extern "C" int app_main()
 {
+    ESP_LOGI(TAG, "Create timer and operator");
+    //Servo init
+    mcpwm_timer_handle_t timer = NULL;
+    mcpwm_timer_config_t timer_config = {
+        .group_id = 0,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+        .period_ticks = SERVO_TIMEBASE_PERIOD,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
+
+    mcpwm_oper_handle_t oper = NULL;
+    mcpwm_operator_config_t operator_config = {
+        .group_id = 0, // operator must be in the same group to the timer
+    };
+    ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &oper));
+
+    ESP_LOGI(TAG, "Connect timer and operator");
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
+
+    ESP_LOGI(TAG, "Create comparator and generator from the operator");
+    mcpwm_cmpr_handle_t comparator = NULL;
+    mcpwm_comparator_config_t comparator_config;
+    comparator_config.flags.update_cmp_on_tez = true;
+    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &comparator_config, &comparator));
+
+    mcpwm_gen_handle_t generator = NULL;
+    mcpwm_generator_config_t generator_config = {
+        .gen_gpio_num = SERVO_PULSE_GPIO,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_generator(oper, &generator_config, &generator));
+
+    // set the initial compare value, so that the servo will spin to the center position
+    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare(0)));
+
+    ESP_LOGI(TAG, "Set generator action on timer and compare event");
+    // go high on counter empty
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(generator,
+                                                              MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
+    // go low on compare threshold
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generator,
+                                                                MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
+
+    ESP_LOGI(TAG, "Enable and start timer");
+    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+
+    //control loop
+    int angle = 0;
+    int step = 5;
+    while (1) {
+        
+        //Add delay, since it takes time for servo to rotate, usually 200ms/60degree rotation under 5V power supply
+        
+        angle =  -38;
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare(angle)));
+        angle = 50;
+        ei_sleep(200);
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare(angle)));
+        ei_sleep(5000);
+    }
     //Camera init
     if(ESP_OK != init_camera()) {
         return ESP_FAIL;
@@ -222,7 +302,7 @@ extern "C" int app_main()
         print_inference_result(result);
 
         gpio_set_level(LED_PIN, 0);
-        ei_sleep(1000);
+        
     }
 }
 
